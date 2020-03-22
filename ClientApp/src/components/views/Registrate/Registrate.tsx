e87cce3023e8270863df521e242944511ec2ae69
Никита Герасимов <nik.gerasimov@inbox.ru>
2020-03-22 18:37:52 +0300

    Добавил валидацию для формы регистрации. Убрал синюю обводку у всех элементов

--- ClientApp/src/components/views/Registrate/Registrate.tsx
+++ ClientApp/src/components/views/Registrate/Registrate.tsx
@@ -33,18 +33,39 @@ class Registrate extends Component<registrateProps, registrateState>{
             isSecondPasswordCorrect: true
         }
         this.submitPassword = this.submitPassword.bind(this);
+        this.validation = this.validation.bind(this);
     }
     submitPassword(event: React.ChangeEvent<HTMLInputElement>) {
         event.persist();
         if (event.target.value === this.state.user.password) {
-            this.setState({ isSecondPasswordCorrect: true });
+            this.setState({ isSecondPasswordCorrect: true }, () => {
+                this.validation()
+            });
         } else {
-            this.setState({ isSecondPasswordCorrect: false });
+            this.setState({ isSecondPasswordCorrect: false }, () => {
+                this.validation()
+            });
         }
     }
     registrateHandler() {
         this.props.registrate(this.state.user);
         this.props.close();
+    }
+    validation() {
+        if (this.state.user.name
+            && this.state.user.surname
+            && this.state.user.e_mail
+            && this.state.user.login
+            && this.state.user.password
+            && this.state.isSecondPasswordCorrect) {
+            this.setState({
+                isFormValid: true
+            });
+        } else {
+            this.setState({
+                isFormValid: false
+            });
+        }
     }
     render() {
         return (
@@ -57,50 +78,63 @@ class Registrate extends Component<registrateProps, registrateState>{
                         <p>Имя</p>
                         <input type="text"
                             className="inp-base"
+                            maxLength={50}
                             onChange={e => {
                                 e.persist();
                                 this.setState(prevState => {
                                     let user = Object.assign({}, prevState.user);
                                     user.name = e.target.value;
                                     return { user };
+                                }, () => {
+                                    this.validation()
                                 })
                             }} />
                         <p>Фамилия</p>
                         <input type="text"
                             className="inp-base"
+                            maxLength={50}
                             onChange={e => {
                                 e.persist()
                                 this.setState(prevState => {
                                     let user = Object.assign({}, prevState.user);
                                     user.surname = e.target.value;
                                     return { user };
+                                }, () => {
+                                    this.validation()
                                 })
                             }} />
                         <p>Электронная поча</p>
                         <input type="text"
                             className="inp-base"
+                            maxLength={50}
                             onChange={e => {
                                 e.persist()
                                 this.setState(prevState => {
                                     let user = Object.assign({}, prevState.user);
                                     user.e_mail = e.target.value;
                                     return { user };
+                                }, () => {
+                                    this.validation()
                                 })
                             }} />
                         <p>Логин</p>
                         <input type="text"
                             className="inp-base"
+                            maxLength={50}
                             onChange={e => {
                                 e.persist()
                                 this.setState(prevState => {
                                     let user = Object.assign({}, prevState.user);
                                     user.login = e.target.value;
                                     return { user };
+                                }, () => {
+                                    this.validation()
                                 })
                             }} />
                         <p>Пароль</p>
-                        <input type="text"
+                        <input type="password"
                             className="inp-base"
+                            maxLength={50}
                             onChange={e => {
                                 e.persist()
                                 this.setState(prevState => {
@@ -110,15 +144,20 @@ class Registrate extends Component<registrateProps, registrateState>{
                                         user,
                                         isSecondPasswordCorrect: false
                                     };
+                                }, () => {
+                                    this.validation()
                                 })
                             }} />
                         <p>Подтвердите пароль</p>
-                        <input type="text"
-                            className="inp-base"
+                        <input type="password"
+                            className={this.state.isSecondPasswordCorrect ?
+                                'inp-base'
+                                : 'inp-base error'}
                             onChange={this.submitPassword} />
                     </div>
                     <button className="btn registrate"
-                        onClick={()=>this.registrateHandler()}>
+                        disabled={!this.state.isFormValid}
+                        onClick={() => this.registrateHandler()}>
                         Зарегистрироваться
                     </button>
                 </div>
